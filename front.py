bf11bf9320ec70f75a0e8546cc92443b6c9dc86d
SehanSardana <128808729+SehanSardana@users.noreply.github.com>
2023-11-08 16:09:36 +0530

    Update front.py

--- front.py
+++ front.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import datetime
 st.title("To-Do List")
+conn = st.connection('mysql', type='sql')
 
 st.sidebar.write("User Authentication")
 
@@ -18,8 +19,8 @@ if st.sidebar.checkbox('Submit'):
         task = st.text_input('Name of task')
         completion_date = st.date_input("When's your task due", datetime.date(2019, 7, 6))
     elif (option == 'Read Task'):
-        st.write("database daalo")
-        st.write("HELOO")
+        df = conn.query('SELECT * from credentails;', ttl=600)
+        st.write(df)
     elif(option == 'Update Task'):
         st.write("dsfdsf")
 
